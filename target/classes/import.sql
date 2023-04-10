e2ff9d4d56380c4589edab9cece31f0d71410edd
Nigel Brennan <nbnigelbrennan@gmail.com>
2023-04-10 22:36:38 +0100

    upload target folder for tests

---
+++ target/classes/import.sql
@@ -0,0 +1,7 @@
+
+-- Adding a few initial products 
+insert into retail_app values(2,'JACKET','2023-02-05', 40.0);
+insert into retail_app values(3,'JUMPER', '2023-02-06', 45.0);
+insert into retail_app values(4,'COAT','2023-02-05', 200.0);
+insert into retail_app values(5,'JEANS','2023-02-10', 25.0);
+

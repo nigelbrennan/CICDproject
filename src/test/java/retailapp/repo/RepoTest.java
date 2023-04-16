892487d2a2550ac59fbec3546ecd7724a76d31af
Nigel Brennan <nbnigelbrennan@gmail.com>
2023-04-16 18:26:02 +0100

    final

--- src/test/java/retailapp/repo/RepoTest.java
+++ src/test/java/retailapp/repo/RepoTest.java
@@ -1,23 +1,23 @@
 package retailapp.repo;
 
 
-//import static org.hamcrest.Matchers.*;
-//import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import retailapp.RetailApplication;
 import retailapp.repos.RetailAppRepo;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest(classes = RetailApplication.class, webEnvironment = WebEnvironment.NONE)
-//@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = RetailApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class RepoTest {
 	
 
